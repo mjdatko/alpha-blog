f95aa5231e795593aa8cd7d65b6c3879ce84c1cd
Mike <mdatko@sbcglobal.net>
2018-01-08 15:52:18 +0000

    Fix tests and update navbar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,26 +2,34 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch:: IntegrationTest
   
-  test "get new category form and create catgory" do
-  get new_category_path
-  assert_template 'categories/new'
-  assert_difference 'Category.count', 1 do
-    post categories_path, params:{category:{name: "sports"}}
-    follow_redirect!
-  end
-  assert_template 'categories/index'
-  assert_match "sports", response.body
+  def setup
+     @user = User.create(username: "mike", email: "mike@example.com", password: "password", admin: true)
   end
   
-  test "invalid category submission results in failure" do 
+  
+  
+  test "get new category form and create catgory" do
+    sign_in_as(@user, "password")
     get new_category_path
-  assert_template 'categories/new'
-  assert_no_difference 'Category.count' do
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
+      post categories_path, params:{category:{name: "sports"}}
+      follow_redirect!
+    end
+    assert_template 'categories/index'
+    assert_match "sports", response.body
+    end
+  
+  test "invalid category submission results in failure" do 
+    sign_in_as(@user, "password")
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
     post categories_path, params:{category:{name: " "}}
     
   end
-  assert_template 'categories/new'
-  assert_select 'h2.panel-title'
-  assert_select 'div.panel-body'
+    assert_template 'categories/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end 
 end
